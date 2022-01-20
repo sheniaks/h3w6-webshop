8ac3fdea90746adfb263063e7f2de8670806dc21
sheniaks <sheniaks@gmail.com>
2022-01-20 12:27:55 +0200

    buttons changed to select form in controller_sort.js + view_sort.js

--- app/sort/controller_sort.js
+++ app/sort/controller_sort.js
@@ -3,16 +3,21 @@ import ViewSort from "./view_sort.js";
 
 export default class ControllerSort{
     constructor(handeSortByControllerCards){
-        this.view = new ViewSort(this.handleClickBtnSort);
+        this.view = new ViewSort(this.handleClickFormSelect, this.handleClickBtnSort);
         this.handeSortByControllerCards = handeSortByControllerCards;
     
         this.view.init();
         this.pub = new Publisher();
     }
 
-    handleClickBtnSort = ev => {
-        const sortType = ev.srcElement.dataset.btn;
+    // handleClickBtnSort = ev => {
+    //     const sortType = ev.srcElement.dataset.btn;
+    //     this.pub.notify('ON_CLICK_SORT', sortType)
+    //    // this.handeSortByControllerCards(sortType);
+    // }
+
+    handleClickFormSelect = ev => {
+        const sortType = ev.target.value;
         this.pub.notify('ON_CLICK_SORT', sortType)
-       // this.handeSortByControllerCards(sortType);
     }
 }
